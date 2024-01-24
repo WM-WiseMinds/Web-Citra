1a23e8d42eea073d35cff5208ef3ccd9b786204c
Borbobb <madeadisujaya23@gmail.com>
2024-01-24 09:39:10 +0800

    Update sidebar

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Roles;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        User::factory(50)->create();
+
+        $user = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('Password'),
+            'role_id' => 1,
+        ]);
+    }
+    
+}
